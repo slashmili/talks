f2197e19ea74a4bb1a60debd6e41c7968b188291
Milad Rastian <milad@high-mobility.com>
2017-11-08 22:20:35 +0100

    add echo server and listen to port 22

--- 2017/ssh-elixir-berin-nov/ssh_talk/lib/ssh_talk/server.ex
+++ 2017/ssh-elixir-berin-nov/ssh_talk/lib/ssh_talk/server.ex
@@ -48,6 +48,40 @@ defmodule SshTalk.Server do
       key_cb: SshTalk.GitHubKeyAuthentication
   end
 
+  def echo_server do
+    :ssh.daemon 22,
+      system_dir: String.to_charlist(@root_dir),
+      user_dir: String.to_charlist(@root_dir),
+      auth_methods: 'publickey',
+      disconnectfun: &log_it/1,
+      connectfun: &log_it/3,
+      failfun: &log_it/3,
+      shell: &echo_shell/2,
+      key_cb: SshTalk.GitHubKeyAuthentication
+  end
+
+  defp echo_shell(username, peer) do
+    username = to_string(username)
+    Logger.debug("echo shell #{inspect username}, #{inspect peer}")
+    loop = fn ->
+      send self(), {:input, self(), IO.gets(:stdio, "#{username}-> ")}
+      receive do
+        {:input, _, "exit\n"} ->
+          IO.puts "Exiting..."
+          :exit
+        {:input, _, message} ->
+          IO.puts String.trim(to_string(message))
+          :contiune
+      end
+    end
+    spawn_link fn ->
+      IO.puts "ECHO SSH shell - presse Ctr+C to quit"
+      1..1
+      |> Stream.cycle
+      |> Enum.any?(fn _ -> loop.() == :exit end)
+    end
+  end
+
   def shell(username, peer) do
     Logger.debug("shell #{inspect username}, #{inspect peer}")
     spawn_link fn ->
